1c99f3d1e47fe3708543fa415f9b80005c93e70c
Dolvic <Dolvic@users.noreply.github.com>
2022-12-10 07:20:00 -0800

    ✨ Implement Day 1, Part 2

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,12 +1,10 @@
-use std::io;
+use std::fs;
 
-pub(crate) fn part_1() {
+pub(crate) fn part_1() -> u32 {
     let mut max_calories = 0;
     let mut current_calories = 0;
 
-    for calories in io::stdin().lines() {
-        let calories = calories.expect("Unable to read calorie input");
-
+    for calories in get_input() {
         if calories.is_empty() {
             max_calories = max_calories.max(current_calories);
             current_calories = 0;
@@ -15,5 +13,28 @@ pub(crate) fn part_1() {
             current_calories += calories;
         }
     }
-    println!("{max_calories}")
+    max_calories
+}
+
+pub(crate) fn part_2() -> u32 {
+    let mut max_calories = [0; 4];
+
+    for calories in get_input() {
+        if calories.is_empty() {
+            max_calories.sort();
+            max_calories[0] = 0;
+        } else {
+            let calories: u32 = calories.parse().expect("Unable to parse calorie input");
+            max_calories[0] += calories;
+        }
+    }
+    max_calories[1..].iter().sum()
+}
+
+fn get_input() -> Vec<String> {
+    fs::read_to_string("inputs/day_1.txt")
+        .expect("Input file should be readable")
+        .lines()
+        .map(|s| s.to_owned())
+        .collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day_1;
 
 fn main() {
-    day_1::part_1();
+    println!("Day 1, Part 1: {}", day_1::part_1());
+    println!("Day 1, Part 2: {}", day_1::part_2());
 }
